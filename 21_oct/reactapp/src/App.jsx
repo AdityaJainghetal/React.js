86505be850ef3e03d99822febed606c66033a07e
Aditya Jain <adityajainghetal@gmail.com>
2024-11-05 15:02:23 +0530

    Project

---
+++ 21_oct/reactapp/src/App.jsx
@@ -0,0 +1,56 @@
+// // import React from "react";
+// const name = "Aditya";
+// const age = 23;
+
+
+
+// const App=()=>{
+//   return(
+
+//     <h1>Welcome to Bhopal {name} We are web are {age*3} developer</h1>
+
+//   )
+// }
+
+// export default App;
+
+
+
+
+// -----------------------
+// import React from "react";
+// const sub=<ol>
+//         <li>PHP</li>
+//         <li>JAVA</li>
+//         <li>JAVASCIPT</li>
+//         <li>ASP</li>
+//         <li>Perl</li>
+
+
+// </ol>
+ 
+// const App=()=>{
+//   return(
+//     <h1>Welcome to Cybrom : {sub}</h1>
+
+//   )
+// }
+
+
+//one top element
+
+import React from "react";
+
+ 
+const App=()=>{
+  return(
+    <>
+    <h1>Welcome to Cybrom</h1>
+    <h2>welcome</h2>
+    <h3>react app</h3>
+    </>
+
+  )
+}
+
+export default App;
---
+++ 22_oct/reactapp/src/cybrom.jsx
@@ -0,0 +1,9 @@
+const College=()=>{
+  retunr(
+    <>
+    <h1>Welcome</h1>
+    </>
+  )   
+}
+
+export default College;
